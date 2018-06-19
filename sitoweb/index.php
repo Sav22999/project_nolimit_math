e33b2ba0d33ca528afe98f3cac771de2d460f6f9
Sav22999 <saverio.morelli@outlook.it>
2018-06-19 13:05:24 +0200

    Aggiunto sito web NoLimit Math

---
+++ sitoweb/index.php
@@ -0,0 +1,80 @@
+<!---
+SITE REALISED BY: SAVERIO MORELLI
+
+> > > www.saveriomorelli.com < < <
+
+> > | www.saveriomorelli.com/nolimitmath/ | < <
+--->
+<html>
+	<head>
+		<link rel="stylesheet" href="/nolimitmath/style.css" />
+		<link rel="icon" href="/nolimitmath/icona.png" />
+		<meta http-equiv="content-type" content="text/html; charset=UTF-16">
+		<meta name="viewport" content="width=device-width, initial-scale=0.8"/>
+		
+		<title>Progetto: NoLimit Math</title>
+	</head>
+	<body>
+		<div id="parte_iniziale">NoLimit Math</div>
+		<div id="vedi_su_github">
+			<input type="button" id="vedi_su_github" onclick="location.href='https://github.com/Sav22999/project_nolimit_math'" value="Vedi il progetto su GitHub" />
+		</div>
+		<div id="loghi">
+			<table id="loghi">
+				<tr>
+					<td><a href="https://www.python.org/"><img src="/nolimitmath/python.png" height="50px" title="Linguaggio utilizzato: Python" /></a></td>
+					<td><a href="https://www.riverbankcomputing.com/software/pyqt/intro"><img src="/nolimitmath/qt5.png" height="50px" title="Grafica del software: PyQt5" /></a></td>
+					<td><a href="https://www.gnu.org/licenses/gpl-3.0.en.html"><img src="/nolimitmath/gplv3.png" height="50px" title="Licenza GNU GPL v3" /></a></td>
+					<td><a href="https://pypi.org/project/nolimit/"><img src="/nolimitmath/pypi.png" height="50px" title="NoLimit Math è presente anche su PyPi" /></a></td>
+				</tr>
+				<tr>
+					<td><a href="https://kubuntu.org/"><img src="/nolimitmath/kubuntu.png" height="50px" title="OS utilizzato: Kubuntu" /></a></td>
+					<td><a href="https://pythonhosted.org/spyder/index.html"><img src="/nolimitmath/spyder.png" height="50px" title="IDE utilizzato: Spyder" /></a></td>
+					<td><a href="https://www.saveriomorelli.com/"><img src="/nolimitmath/smrealised.png" height="50px" title="Realizzazione SM" /></a></td>
+					<td><a href="https://www.saveriomorelli.com/"><img src="/nolimitmath/smproject.png" height="50px" title="Progetto SM" /></a></td>
+				</tr>
+			</table>
+		</div>
+		<div id="descrizione">
+			NoLimit Math è un software open-source e free, sviluppato principalmente da Saverio Morelli, ma anche da tutti coloro che collaborano con egli.
+			<br/>
+			Il linguaggio di programmazione utilizzato è il Python (versione 3.6) con GUI PyQt5.
+			<br/>
+			<br/>
+			NoLimit Math è presente anche su PyPi ed è possibile installare la libreria semplicemente eseguendo il comando "pip install nolimit".
+			<br/>
+			<br/>
+			Il software permette di calcolare e di generare il grafico di un limite "semplice", ovvero di una funzione "(ax<sup>2</sup>+bx+c)/(ax<sup>2</sup>+bx+c)" oppure che si presenta come forma indeterminata "∞/∞".
+			<br/>
+			Se, invece, si utilizza la versione Sympy si può eseguire qualsiasi funzione.
+			<hr color="white">
+			<small>Per maggiori informazioni leggere il README presente in GitHub.</small>
+		</div>
+		<div id="librerie">
+			Librerie utilizzate:
+			<br>
+			<table id="librerie">
+				<tr>
+					<td><a href="https://www.riverbankcomputing.com/software/pyqt/intro"><img src="/nolimitmath/pyqt5.png" height="50px" title="PyQt5" /></a></td>
+					<td><a href="https://matplotlib.org/"><img src="/nolimitmath/matplotlib.png" height="50px" title="MatplotLib" /></a></td>
+					<td><a href="http://www.sympy.org/en/index.html"><img src="/nolimitmath/sympy.png" height="50px" title="SymPy" /></a></td>
+					<td><a href="http://www.numpy.org/"><img src="/nolimitmath/numpy.png" height="50px" title="NumPy" /></a></td>
+				</tr>
+			</table>
+		</div>
+		<div id="ringraziamenti">
+			Un ringraziamento a tutti coloro che collaborano al progetto e che lo rendono sempre migliore.
+			<br>
+			Un ringraziamento speciale va a <b>Simone Massaro</b> che ha collaborato in modo significativo al progetto.
+			<hr color="white">
+			<small>
+				Il progetto, i contenuti (le immagini, i video, il testo) e tutto ciò che è contenuto in questa pagina e nel progetto NoLimit Math è sotto licenza GNU GPL v.3. Chiunque, pertanto, è libero di riutilizzare il codice sorgente, inserendo comunque la fonte e utilizzando la stessa licenza.
+			</small>
+		</div>
+	</body>
+</html>
+<!---
+SITE REALISED BY: SAVERIO MORELLI
+
+> > > www.saveriomorelli.com < < <
+--->
