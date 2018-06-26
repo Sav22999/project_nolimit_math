93bc8711885b130d490ba94b24945dc45b1817b5
Saverio Morelli <32335378+Sav22999@users.noreply.github.com>
2018-06-26 16:49:15 +0200

    Aggiornato a v1.1B
    #4

--- nolimit/main.py
+++ nolimit/main.py
@@ -12,7 +12,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 UI_FILE  = os.path.join(os.path.dirname(__file__), "./nolimit.ui")
-textVersione="1.0.1β"
+textVersione="1.1β"
 
 
 def calcolaNumDen(testo, x0, infinito):
@@ -144,162 +144,162 @@ class Ui_noLimit(QtWidgets.QMainWindow, uic.loadUiType(UI_FILE)[0]):
 
     def liveEdit(self):
         if self.checkLive.isChecked():
-            try:
-                self.onClickCalcola()
-            except:
-                print("!! General error: n001 !!")
+            self.onClickCalcola()
 
     def onClickCalcola(self):
-        numeratore = 0
-        denominatore = 1
-        x0 = 0
-        risultato='-'
-        stringaX=self.textX.text()
-        if "+-" in stringaX: stringaX = stringaX.replace("+-", "±")
-        if "-+" in stringaX: stringaX = stringaX.replace("-+", "∓")
-        if "±" in stringaX: stringaX = stringaX.replace("±", "")
-        if "∓" in stringaX: stringaX = stringaX.text().replace("∓", "")
-        if stringaX == "inf" or stringaX == "∞": stringaX = "+inf"
-        if "inf" in stringaX: stringaX=stringaX.replace("inf", "∞")
-        self.textX.setText(stringaX)
-
-        if stringaX!="" and self.textNumeratore.text()!="" and self.textDenominatore.text()!="" and self.textDenominatore.text()!="0":
-            self.bttCalcola.setGeometry(10, 270, 341, 61)
-            self.bttVediGrafico.show()
-            self.line_2.show()
+        try:
+            numeratore = 0
+            denominatore = 1
+            x0 = 0
+            risultato='-'
+            stringaX=self.textX.text()
+            if "+-" in stringaX: stringaX = stringaX.replace("+-", "±")
+            if "-+" in stringaX: stringaX = stringaX.replace("-+", "∓")
+            if "±" in stringaX: stringaX = stringaX.replace("±", "")
+            if "∓" in stringaX: stringaX = stringaX.text().replace("∓", "")
+            if stringaX == "inf" or stringaX == "∞": stringaX = "+inf"
+            if "inf" in stringaX: stringaX=stringaX.replace("inf", "∞")
+            self.textX.setText(stringaX)
+    
+            if stringaX!="" and self.textNumeratore.text()!="" and self.textDenominatore.text()!="" and self.textDenominatore.text()!="0":
+                self.bttCalcola.setGeometry(10, 270, 341, 61)
+                self.bttVediGrafico.show()
+                self.line_2.show()
+                    
+                css_txt = "QLineEdit{background-color:transparent;color:darkred;border:0px solid transparent;border-bottom:1px solid darkred;opacity:.7;font-family:Rockwell;} QLineEdit:focus{border-bottom:2px solid red;opacity:1;background-color: transparent;}"
+                self.textX.setStyleSheet(css_txt)
+                self.textNumeratore.setStyleSheet(css_txt)
+                self.textDenominatore.setStyleSheet(css_txt)
+    
+                if stringaX != "":
+                    x0=calcolaX0(self, stringaX)
+                risultato = '0'
+    
+                if "∞" not in self.textX.text():
+                    if self.textNumeratore.text() != "":
+                        print("Numerator: OK")
+                        numeratore = calcolaNumDen(self.textNumeratore.text(), x0, False)
+                        # print(numeratore)
+    
+                    if self.textDenominatore.text() != "" and self.textDenominatore.text() != "0":
+                        if (self.textDenominatore.text() == "1"):
+                            print("Denominator: OK | Static (1)")
+                        else:
+                            print("Denominator: OK | Custom")
+                            denominatore = calcolaNumDen(self.textDenominatore.text(), x0, False)
+                            # print(denominatore)
+    
+                    if denominatore != 0:
+                        if numeratore != 0:
+                            risultato = numeratore / denominatore
+                    else:
+                        if numeratore != 0:
+                            denDes = calcolaNumDen(self.textDenominatore.text(), (x0 + 0.00001), False)
+                            # denSin=calcolaNumDen(self.textDenominatore.text(), (x0 - 0.00001), False)
+                            self.textX.setText(stringaX + "±")
+                            if denDes < 0:
+                                if numeratore < 0:
+                                    risultato = "±∞"
+                                else:
+                                    risultato = "∓∞"
+                            else:
+                                if numeratore > 0:
+                                    risultato = "±∞"
+                                else:
+                                    risultato = "∓∞"
+                else:
+                    if "x^2" in self.textNumeratore.text():
+                        if "x^2" in self.textDenominatore.text():
+                            aN = calcolaNumDen(self.textNumeratore.text(), x0, True)
+                            aD = calcolaNumDen(self.textDenominatore.text(), x0, True)
+                            risultato = aN / aD
+                        else:
+                            segnoNum=calcolaNumDen(self.textNumeratore.text(), x0, False)
+                            segnoDen=calcolaNumDen(self.textDenominatore.text(), x0, False)
+                            if(segnoNum>0 and segnoDen>0 or segnoNum<0 and segnoDen<0):
+                                risultato = "+∞"
+                            else:
+                                risultato = "-∞"
+                    elif "x" in self.textNumeratore.text():
+                        if "x^2" in self.textDenominatore.text():
+                            segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
+                            segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
+                            risultato=0
+                        elif "x" in self.textDenominatore.text():
+                            bN = calcolaNumDen(self.textNumeratore.text(), x0, True)
+                            bD = calcolaNumDen(self.textDenominatore.text(), x0, True)
+                            risultato = bN / bD
+                        else:
+                            segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
+                            segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
+                            if (segnoNum > 0 and segnoDen > 0 or segnoNum < 0 and segnoDen < 0):
+                                risultato = "+∞"
+                            else:
+                                risultato = "-∞"
+                    else:
+                        if "x^2" in self.textDenominatore.text() or "x" in self.textDenominatore.text():
+                            segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
+                            segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
+                            risultato=0
+                        else:
+                            cN = calcolaNumDen(self.textNumeratore.text(), x0, True)
+                            cD = calcolaNumDen(self.textDenominatore.text(), x0, True)
+                            risultato = cN / cD
+                self.textRisultato.setText(str(risultato))
                 
-            css_txt = "QLineEdit{background-color:transparent;color:darkred;border:0px solid transparent;border-bottom:1px solid darkred;opacity:.7;font-family:Rockwell;} QLineEdit:focus{border-bottom:2px solid red;opacity:1;background-color: transparent;}"
-            self.textX.setStyleSheet(css_txt)
-            self.textNumeratore.setStyleSheet(css_txt)
-            self.textDenominatore.setStyleSheet(css_txt)
-
-            if stringaX != "":
-                x0=calcolaX0(self, stringaX)
-            risultato = '0'
-
-            if "∞" not in self.textX.text():
-                if self.textNumeratore.text() != "":
-                    print("Numerator: OK")
-                    numeratore = calcolaNumDen(self.textNumeratore.text(), x0, False)
-                    # print(numeratore)
-
-                if self.textDenominatore.text() != "" and self.textDenominatore.text() != "0":
-                    if (self.textDenominatore.text() == "1"):
-                        print("Denominator: OK | Static (1)")
+                valNum=self.textNumeratore.text()
+                valDen=self.textDenominatore.text()
+                """print("Numerator:")
+                print(valDen)
+                print("Denominator:")
+                print(valDen)"""
+                i = 0
+                listX1=[]
+                listX2=[]
+                listY1=[]
+                listY2=[]
+                for count in range(0,100):
+                    try:
+                        num = float(calcolaNumDen(valNum, (x0 + i), False) / calcolaNumDen(valDen, (x0 + i), False))
+                        num = float(calcolaNumDen(valNum, (x0 - i), False) / calcolaNumDen(valDen, (x0 - i), False))
+                    except ZeroDivisionError:
+                        i += 0.01
+                    listX1.append(x0 + i)
+                    listY1.append(calcolaNumDen(valNum, (x0 + i), False)/calcolaNumDen(valDen, (x0 + i), False))
+                    listX2.append(x0 - i)
+                    listY2.append(calcolaNumDen(valNum, (x0 - i), False)/calcolaNumDen(valDen, (x0 - i), False))
+                    if x0==9999 or x0==-9999:
+                        i += 100
                     else:
-                        print("Denominator: OK | Custom")
-                        denominatore = calcolaNumDen(self.textDenominatore.text(), x0, False)
-                        # print(denominatore)
-
-                if denominatore != 0:
-                    if numeratore != 0:
-                        risultato = numeratore / denominatore
-                else:
-                    if numeratore != 0:
-                        denDes = calcolaNumDen(self.textDenominatore.text(), (x0 + 0.00001), False)
-                        # denSin=calcolaNumDen(self.textDenominatore.text(), (x0 - 0.00001), False)
-                        self.textX.setText(stringaX + "±")
-                        if denDes < 0:
-                            if numeratore < 0:
-                                risultato = "±∞"
-                            else:
-                                risultato = "∓∞"
-                        else:
-                            if numeratore > 0:
-                                risultato = "±∞"
-                            else:
-                                risultato = "∓∞"
+                        i += 0.05
+                
+                self.plot_canvas.axes.cla()
+                self.plot_canvas.axes.plot([min([min(listX1),min(listX2)]),-1,0,1,max([max(listX1),max(listX2)])], [0,0,0,0,0], 'r', color='black')
+                self.plot_canvas.axes.plot([0,0,0,0,0], [min([min(listY1),min(listY2)]),-1,0,1,max([max(listY1),max(listY2)])], 'r', color='black')
+                self.plot_canvas.axes.plot(listX1,listY1, 'r', color='red')
+                self.plot_canvas.axes.plot(listX2,listY2, 'r', color='red')
+                self.plot_canvas.draw()
+    
+                self.bttVediGrafico.show()
             else:
-                if "x^2" in self.textNumeratore.text():
-                    if "x^2" in self.textDenominatore.text():
-                        aN = calcolaNumDen(self.textNumeratore.text(), x0, True)
-                        aD = calcolaNumDen(self.textDenominatore.text(), x0, True)
-                        risultato = aN / aD
-                    else:
-                        segnoNum=calcolaNumDen(self.textNumeratore.text(), x0, False)
-                        segnoDen=calcolaNumDen(self.textDenominatore.text(), x0, False)
-                        if(segnoNum>0 and segnoDen>0 or segnoNum<0 and segnoDen<0):
-                            risultato = "+∞"
-                        else:
-                            risultato = "-∞"
-                elif "x" in self.textNumeratore.text():
-                    if "x^2" in self.textDenominatore.text():
-                        segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
-                        segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
-                        risultato=0
-                    elif "x" in self.textDenominatore.text():
-                        bN = calcolaNumDen(self.textNumeratore.text(), x0, True)
-                        bD = calcolaNumDen(self.textDenominatore.text(), x0, True)
-                        risultato = bN / bD
-                    else:
-                        segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
-                        segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
-                        if (segnoNum > 0 and segnoDen > 0 or segnoNum < 0 and segnoDen < 0):
-                            risultato = "+∞"
-                        else:
-                            risultato = "-∞"
-                else:
-                    if "x^2" in self.textDenominatore.text() or "x" in self.textDenominatore.text():
-                        segnoNum = calcolaNumDen(self.textNumeratore.text(), x0, False)
-                        segnoDen = calcolaNumDen(self.textDenominatore.text(), x0, False)
-                        risultato=0
-                    else:
-                        cN = calcolaNumDen(self.textNumeratore.text(), x0, True)
-                        cD = calcolaNumDen(self.textDenominatore.text(), x0, True)
-                        risultato = cN / cD
-            self.textRisultato.setText(str(risultato))
-            
-            valNum=self.textNumeratore.text()
-            valDen=self.textDenominatore.text()
-            """print("Numerator:")
-            print(valDen)
-            print("Denominator:")
-            print(valDen)"""
-            i = 0
-            listX1=[]
-            listX2=[]
-            listY1=[]
-            listY2=[]
-            for count in range(0,100):
-                try:
-                    num = float(calcolaNumDen(valNum, (x0 + i), False) / calcolaNumDen(valDen, (x0 + i), False))
-                    num = float(calcolaNumDen(valNum, (x0 - i), False) / calcolaNumDen(valDen, (x0 - i), False))
-                except ZeroDivisionError:
-                    i += 0.01
-                listX1.append(x0 + i)
-                listY1.append(calcolaNumDen(valNum, (x0 + i), False)/calcolaNumDen(valDen, (x0 + i), False))
-                listX2.append(x0 - i)
-                listY2.append(calcolaNumDen(valNum, (x0 - i), False)/calcolaNumDen(valDen, (x0 - i), False))
-                if x0==9999 or x0==-9999:
-                    i += 100
-                else:
-                    i += 0.05
-            
-            self.plot_canvas.axes.cla()
-            self.plot_canvas.axes.plot([min([min(listX1),min(listX2)]),-1,0,1,max([max(listX1),max(listX2)])], [0,0,0,0,0], 'r', color='black')
-            self.plot_canvas.axes.plot([0,0,0,0,0], [min([min(listY1),min(listY2)]),-1,0,1,max([max(listY1),max(listY2)])], 'r', color='black')
-            self.plot_canvas.axes.plot(listX1,listY1, 'r', color='red')
-            self.plot_canvas.axes.plot(listX2,listY2, 'r', color='red')
-            self.plot_canvas.draw()
-
-            self.bttVediGrafico.show()
-        else:
-            css_txt = "QLineEdit{background-color:transparent;color:darkred;border:0px solid transparent;border-bottom:1px solid darkred;opacity:.7;font-family:Rockwell;} QLineEdit:focus{border-bottom:2px solid red;opacity:1;background-color: transparent;}"
-            self.textX.setStyleSheet(css_txt)
-            self.textNumeratore.setStyleSheet(css_txt)
-            self.textDenominatore.setStyleSheet(css_txt)
-            if stringaX=="":
-                print("X(0): Error | Empty")
-                self.textX.setStyleSheet(css_txt.replace("background-color:transparent;","background-color:red;"))
-            if self.textNumeratore.text()=="":
-                print("Numerator: Error | Empty")
-                self.textNumeratore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
-            if self.textDenominatore.text()=="":
-                print("Denominator: Error | Empty")
-                self.textDenominatore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
-            if self.textDenominatore.text()=="0":
-                print("Denominator: Error | ZeroDivision")
-                self.textDenominatore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
+                css_txt = "QLineEdit{background-color:transparent;color:darkred;border:0px solid transparent;border-bottom:1px solid darkred;opacity:.7;font-family:Rockwell;} QLineEdit:focus{border-bottom:2px solid red;opacity:1;background-color: transparent;}"
+                self.textX.setStyleSheet(css_txt)
+                self.textNumeratore.setStyleSheet(css_txt)
+                self.textDenominatore.setStyleSheet(css_txt)
+                if stringaX=="":
+                    print("X(0): Error | Empty")
+                    self.textX.setStyleSheet(css_txt.replace("background-color:transparent;","background-color:red;"))
+                if self.textNumeratore.text()=="":
+                    print("Numerator: Error | Empty")
+                    self.textNumeratore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
+                if self.textDenominatore.text()=="":
+                    print("Denominator: Error | Empty")
+                    self.textDenominatore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
+                if self.textDenominatore.text()=="0":
+                    print("Denominator: Error | ZeroDivision")
+                    self.textDenominatore.setStyleSheet(css_txt.replace("background-color:transparent;", "background-color:red;"))
+            except:
+                print("!! General error: n001 !!")
 
     def onClickVediGrafico(self):
         stringaX = self.textX.text()
